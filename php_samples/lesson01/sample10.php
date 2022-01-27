11d01048b887edc6fb2443c639919f8d3366e06e
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-01-27 18:02:22 +0900

    array

--- php_samples/lesson01/sample10.php
+++ php_samples/lesson01/sample10.php
@@ -16,12 +16,11 @@
 	<!--<h2>Practice</h2>-->
 <pre>
 <?php
+$week_name = ['日', '月', '火', '水', '木', '金', '土'];
+
 date_default_timezone_set('Asia/Tokyo');
-for ($i = 0; $i < 366; $i++){
-	$time = strtotime("+ {$i} day");
-	$day = date('n/j(D)', $time);
-	echo $day. "\n";
-}
+$week = date('w');
+echo "今日は、$week_name[$week]曜日です";
 ?>
 </pre>
 </main>
