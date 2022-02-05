0f236a72c58385a2f11c0e2ae9e46ca5bbff4575
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-02-05 22:33:30 +0900

    file_put_contents

--- php_samples/lesson01/sample15.php
+++ php_samples/lesson01/sample15.php
@@ -16,7 +16,13 @@
 	<!--<h2>Practice</h2>-->
 <pre>
 <?php
-
+$success = file_put_contents("data/news.txt",
+"ホームページをリニューアルしました");
+if ($success !== false) {
+	echo "ファイルの書き込みが完了しました";
+} else {
+	echo "ファイルの書き込みに失敗しました";
+}
 ?>
 </pre>
 </main>
