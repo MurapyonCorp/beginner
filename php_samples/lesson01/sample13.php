cb68c37dec7662db7c6d1a53832fe54ccb2888d6
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-02-05 22:15:42 +0900

    round

--- php_samples/lesson01/sample13.php
+++ php_samples/lesson01/sample13.php
@@ -15,8 +15,14 @@
 <main>
 	<!--<h2>Practice</h2>-->
 <pre>
+3,000円の物から、100円値引きした場合、
 <?php
-
+echo floor(100 / 3000 * 100);
+?>%引きです
+<br>
+<?php
+echo ceil(3.33). "\n";
+echo round(3.66, 1). "\n";
 ?>
 </pre>
 </main>
