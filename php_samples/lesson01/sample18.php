a3ed2161ea7c183d241bdf107a3ac0f2c6b5ed0b
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-02-07 16:22:24 +0900

    json decode

--- php_samples/lesson01/sample18.php
+++ php_samples/lesson01/sample18.php
@@ -16,7 +16,14 @@
 	<!--<h2>Practice</h2>-->
 <pre>
 <?php
+$file = file_get_contents("feed.json");
+$json = json_decode($file);
 
+foreach ($json -> items as $item) {
+?>
+・<a href="<?php echo $item -> url; ?>"><?php echo $item -> title; ?></a><br>
+<?php
+}
 ?>
 </pre>
 </main>
