3bacc76d8a099c8cff2250cd833d3c3fe93fde51
Ryoooo0811 <ryoooo3286stormedge@outlook.jp>
2022-04-05 23:40:39 +0900

    unit 40

---
+++ php_samples/lesson01/sample31/page01.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<link rel="stylesheet" href="../../css/style.css" />
+	<title>PHP</title>
+</head>
+<body>
+<header>
+	<!--<h1 class="font-weight-normal">PHP</h1>    -->
+</header>
+
+<main>
+	<!--<h2>Practice</h2>-->
+<pre>
+<?php
+	require('intax.php');
+	$price = 150;
+	$price_tax = intax($price);
+	echo $price_tax;
+?>
+</pre>
+</main>
+</body>
+</html>
